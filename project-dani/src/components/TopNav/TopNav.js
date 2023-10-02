faced2a26e0027a499dbcb45b9ab8350c589a37b
Daniel <car15114@byui.edu>
2023-10-02 13:48:06 +0200

    started implementing Routes. For now, only applied to Register link

--- project-dani/src/components/TopNav/TopNav.js
+++ project-dani/src/components/TopNav/TopNav.js
@@ -1,4 +1,6 @@
+import Register from "../Register/Register";
 import "./topnav.css";
+import { Routes, Route, Link } from "react-router-dom";
 
 
 const TopNav=()=>{
@@ -16,10 +18,10 @@ const TopNav=()=>{
                                 </div>
                                 <div class="col-xs-10 text-right menu-1">
                                     <ul>
-                                        <li class="active"><a href="index.html">Home</a></li>
-                                        <li><a href="work.html">Work</a></li>
+                                        <li class="active"><Link to="#">Home</Link></li>
+                                        <li><a href="#">Work</a></li>
                                         <li class="has-dropdown">
-                                            <a href="blog.html">Blog</a>
+                                            <a href="#">Blog</a>
                                             <ul class="dropdown">
                                                 <li><a href="#">Web Design</a></li>
                                                 <li><a href="#">eCommerce</a></li>
@@ -27,15 +29,21 @@ const TopNav=()=>{
                                                 <li><a href="#">API</a></li>
                                             </ul>
                                         </li>
-                                        <li><a href="about.html">About</a></li>
-                                        <li><a href="contact.html">Contact</a></li>
+                                        <li><a href="#">About</a></li>
+                                        <li><a href="#">Contact</a></li>
+                                        <li id="register"><Link to="/register">Register</Link></li>
                                     </ul>
+                                    
                                 </div>
                             </div>
                         </div>
                     </div>
                 </nav>
             </div>   
+
+            <Routes>
+                <Route path="/register" element={<Register />}/>
+            </Routes>
         </div>
     )
 }
