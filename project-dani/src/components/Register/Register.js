21926ccc9ae66aabf239c9cd94ba6d708d8a38e5
Daniel <car15114@byui.edu>
2023-10-04 23:02:32 +0200

    imported register.css | removed useEffect() and replaced it for getUser() and called it on users button | added add and users buttons

--- project-dani/src/components/Register/Register.js
+++ project-dani/src/components/Register/Register.js
@@ -1,13 +1,10 @@
 import { useEffect, useState } from "react";
 import {Table, Button} from "react-bootstrap";
+import "./register.css";
 
 
 function Register(){    
     const [data,setData] = useState(null);
-    
-    useEffect(()=>{
-        getUser();
-    },2000)
       
     const getUser=()=>{
         fetch("http://localhost:3000/userBase/")
@@ -21,29 +18,36 @@ function Register(){
     }
                 
     return( 
-        <Table striped bordered hover>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Options</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    data &&
-                    data.map((user)=>{
-                        return(
-                            <tr>
-                                <td>{user.name}</td>
-                                <td>{user.mail}</td>
-                                <td>Update <Button variant="danger" onClick={()=>deleteUser(user.id)}>Delete</Button></td>
-                            </tr>
-                        )
-                    })
-                }
-            </tbody>
-        </Table>
+        <div>
+            <div className="button-group">
+                <Button variant="primary" className="btn">Add</Button>
+                <Button variant="primary" className="btn" onClick={()=>getUser()}>Users</Button>
+            </div>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Options</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        data &&
+                        data.map((user)=>{
+                            return(
+                                <tr>
+                                    <td>{user.name}</td>
+                                    <td>{user.mail}</td>
+                                    <td>Update <Button variant="danger" onClick={()=>deleteUser(user.id)}>Delete</Button></td>
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </Table>
+        </div>
+        
     );
 }
 
