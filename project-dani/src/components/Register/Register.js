7602cbc41b8172a8691dc58894c00976105ef2a8
Daniel <car15114@byui.edu>
2023-10-04 12:49:28 +0200

    implemented a simple table to render the user data (for now, just testing) | implemented simple CRUD Get and DEELETE methods. It is working

--- project-dani/src/components/Register/Register.js
+++ project-dani/src/components/Register/Register.js
@@ -1,10 +1,55 @@
+import { useEffect, useState } from "react";
+import {Table, Button} from "react-bootstrap";
+import FetchUser from "../customHoolks/fetchUser";
+import DeleteUser from "../customHoolks/deleteUser";
 
-const Register=()=>{
-    return(
-        <div>
-            <h1>Register Component</h1>
-        </div>
-    )
+function Register(){    
+    const [data,setData] = useState(null);
+
+   
+        useEffect(()=>{
+            fetch("http://localhost:3000/userBase")
+            .then((response)=>response.json())
+            .then((data)=> setData(data))
+        },2000)
+      
+    
+    
+
+    const deleteUser=(id)=>{
+        fetch("http://localhost:3000/userBase/"+id, { method : "DELETE"})
+        .then((response)=>{
+            if(response.ok){
+               
+            }
+        })
+    }
+                
+    return( 
+        <Table striped bordered hover>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Options</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    data &&
+                    data.map((user)=>{
+                        return(
+                            <tr>
+                                <td>{user.name}</td>
+                                <td>{user.mail}</td>
+                                <td>Update <Button variant="danger" onClick={()=>deleteUser(user.id)}>Delete</Button></td>
+                            </tr>
+                        )
+                    })
+                }
+            </tbody>
+        </Table>
+    );
 }
 
 export default Register;
