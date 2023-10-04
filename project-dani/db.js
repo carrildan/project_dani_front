143344f9b8fd5261f615dc8efcda2ffb259c50de
Daniel <car15114@byui.edu>
2023-10-04 12:58:37 +0200

    database for initial testing

---
+++ project-dani/db.js
@@ -0,0 +1,58 @@
+[
+    {
+        "id": 1,
+        "name": "Felipe",
+        "mail": "felipe@mail.com",
+        "phoneNumber": "604000012",
+        "linkedin": "www.linkdin.com/felipe",
+        "github": null,
+        "links": [
+            {
+                "rel": "self",
+                "href": "http://localhost:8001/userbase/1"
+            }
+        ]
+    },
+    {
+        "id": 2,
+        "name": "Daniel",
+        "mail": "Daniel@mail.com",
+        "phoneNumber": "604000012",
+        "linkedin": "www.linkdin.com/Daniel",
+        "github": null,
+        "links": [
+            {
+                "rel": "self",
+                "href": "http://localhost:8001/userbase/2"
+            }
+        ]
+    },
+    {
+        "id": 3,
+        "name": "Domingos",
+        "mail": "Domingos@mail.com",
+        "phoneNumber": "604000012",
+        "linkedin": "www.linkdin.com/Domingos",
+        "github": null,
+        "links": [
+            {
+                "rel": "self",
+                "href": "http://localhost:8001/userbase/3"
+            }
+        ]
+    },
+    {
+        "id": 4,
+        "name": "null",
+        "mail": "felipe@mail.com",
+        "phoneNumber": "604000012",
+        "linkedin": "www.linkdin.com/felipe",
+        "github": null,
+        "links": [
+            {
+                "rel": "self",
+                "href": "http://localhost:8001/userbase/4"
+            }
+        ]
+    }
+]
