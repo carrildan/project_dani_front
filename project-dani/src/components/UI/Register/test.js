b0ba56fe558d9ec5ed61dc7371d04f25ed7e039e
Daniel <car15114@byui.edu>
2023-10-30 14:44:15 +0100

    created test.js to change code to fit dependency injection concept. I will move it to the Register component shortly

--- project-dani/src/components/UI/Register/test.js
+++ project-dani/src/components/UI/Register/test.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import UserLocalStorage from '../../../services/userLocalStorage';
+import React, { useContext, useEffect, useState } from 'react';
+import UserTable from '../UserTable/UserTable';
+import { Button } from 'react-bootstrap';
 
 const Test = () => {
+    const [showUsers, setShowUsers] = useState(false)
     return (
         <div>
-            <h1>Test</h1>
-            <UserLocalStorage />
+            <Button variant="primary" className="btn" onClick={()=>setShowUsers(!showUsers)}>Users</Button>
+            <Button variant="primary" className="btn">New</Button>
+            {
+                [showUsers ? <UserTable/> : null]
+            }
         </div>
     );
 }
