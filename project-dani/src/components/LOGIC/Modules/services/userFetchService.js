515b340449b01346c3f6d80ed812f8da8e4993ee
Daniel <car15114@byui.edu>
2023-11-01 09:29:09 +0100

    united userOperations.js to UserFetchService as a single service | Now UserFetchService will fetch data and handle crud methods

--- project-dani/src/components/LOGIC/Modules/services/userFetchService.js
+++ project-dani/src/components/LOGIC/Modules/services/userFetchService.js
@@ -1,17 +1,30 @@
 //this service module fetches the user crud operations
-export const UserFetchService = async () => {
-        
-        try {
-          const response = await fetch("http://localhost:3000/userBase/");
-          if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-          }
-          const data = await response.json();
-          return data;
-          
-        } catch (error) {
-          console.error("Error fetching data:", error);
-          return null; // Handle the error or return an appropriate value
+export const UserFetchService = {
+  GetAll: async ()=>{
+   
+      try {
+        const response = await fetch("http://localhost:3000/userBase/");
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
         }
+        const data = await response.json();
+        return data;
+        
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        return null; // Handle the error or return an appropriate value
+      }
+  },
+
+  GetById: async ()=>{
+    
+  }
+        
 };
-      
+       /* GetbyId: */
+   
+    /* Update: */
+
+    /* Post: */
+
+    /* Delete */
