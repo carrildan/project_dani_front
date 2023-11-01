2638ef4416f7ca2ca52bd7affc4a14b243217ce5
Daniel <car15114@byui.edu>
2023-11-01 09:47:52 +0100

    added toggle effect to render UserTable component when user click on Show button

--- project-dani/src/components/UI/Register/test.js
+++ project-dani/src/components/UI/Register/test.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import UserTable from '../UserTable/UserTable';
-import { Button } from 'react-bootstrap';
+import { Button, Table } from 'react-bootstrap';
+import { UserProvider } from '../../LOGIC/store/Provider/userProvider';
 
 const Test = () => {
     const [showUsers, setShowUsers] = useState(false)
@@ -9,7 +10,23 @@ const Test = () => {
             <Button variant="primary" className="btn" onClick={()=>setShowUsers(!showUsers)}>Users</Button>
             <Button variant="primary" className="btn">New</Button>
             {
-                [showUsers ? <UserTable/> : null]
+               
+                [
+                    showUsers ?  <UserProvider><UserTable/></UserProvider> /* providing user data only to UserTable */
+                
+                    :
+
+                    <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Options</th>
+                        </tr>
+                    </thead>
+                    </Table>
+                ]
+            
             }
         </div>
     );
