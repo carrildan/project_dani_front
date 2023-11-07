117e0a1ee0f617f56f7d72aad15bb67366b760da
Daniel <car15114@byui.edu>
2023-11-07 21:48:34 +0100

    started implementing PUT method

--- project-dani/src/components/LOGIC/Modules/services/userFetchService.js
+++ project-dani/src/components/LOGIC/Modules/services/userFetchService.js
@@ -1,5 +1,6 @@
 //this service module fetches the user crud operations
 export const UserFetchService = {
+  //return all user data
   GetAll: async ()=>{
    
       try {
@@ -11,13 +12,30 @@ export const UserFetchService = {
         return data;
         
       } catch (error) {
-        console.error("Error fetching data:", error);
+        console.error("Error fetching user data:", error);
         return null; // Handle the error or return an appropriate value
       }
   },
 
-  GetById: async ()=>{
-    
+  //return user data by id
+  GetById: async (id)=>{
+    try{
+      const response = await fetch("http://localhost:3000/userBase/"+id, {
+        method : "PUT",
+        headers : {"Content-Type" : "application/json"},
+        body : JSON.stringify(id)
+      }
+      );
+      if(!response.ok){
+        throw new Error(`HTTP error! Status: ${response.status}`)
+      }
+      const data = await response.json();
+      return data;
+
+    } catch{
+      console.error("Error fetching user data id:", error);
+      return null; 
+    }
   }
         
 };
