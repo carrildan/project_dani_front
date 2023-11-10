d656298290e5fc8d1678e51bd900397ca3cecd21
Daniel <car15114@byui.edu>
2023-11-10 19:02:19 +0100

    implemented update user functionality. By now it is inside the UserForm.js but I will moved to a separated file | changed the way value receives users data | also started implementing onClick button for save changes | updated comments for better documentation

--- project-dani/src/components/UI/Forms/userForm/userForm.js
+++ project-dani/src/components/UI/Forms/userForm/userForm.js
@@ -1,10 +1,29 @@
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Button, Modal, Form } from "react-bootstrap";
+import { UserFetchService } from "../../../LOGIC/Modules/services/userFetchService";
 
 const UserForm = (props)=>{
-    //toggles the close button on the form
-    const [showModal, setShowModal] = useState(props);
-    console.log(showModal,"user form")
+    const [showModal, setShowModal] = useState(props.value); //receives true from UserTable.js to open the modal
+    const [updatedUser, setUpdatedUser] = useState()
+    
+    //handle the async process when receives the user data, making sure the compon renders with the correct data
+    useEffect(()=>{
+        setUpdatedUser(props.user || {});
+    }, [props.user])
+  
+    //handlers the update data of an user
+    const updateHandler = async () =>{
+        try{
+            // Use the update method from UserFetchService to update the user
+            await UserFetchService.update(updatedUser.id, updatedUser);
+
+            // Close the modal when clicked save changes button
+            props.onClose()
+        } catch (error) {
+            console.error("Error updating user:", error)
+        }
+    };
+
 
     return(
         <div>
@@ -17,11 +36,11 @@ const UserForm = (props)=>{
                     <Form>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Id</Form.Label>
-                            <Form.Control type="text" /* value={newUser.id} */ readOnly={true} />
+                            <Form.Control type="text" value={updatedUser?.id || " "} readOnly={true} />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Full Name</Form.Label>
-                            <Form.Control type="text" placeholder="Enter name" /* value={newUser.name} */ /* onChange={(e)=>nameHandler(e)} */ />
+                            <Form.Control type="text" placeholder="Enter name" value={updatedUser?.name} /* onChange={(e)=>nameHandler(e)} */ />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
@@ -50,13 +69,13 @@ const UserForm = (props)=>{
 
                 <Modal.Footer>
                     <Button variant="secondary" onClick={()=>{
-                            //set modal off when click on close button. Also informs UserTable the form is closed, otherwise it would still sing as open 
+                            //set modal off when click on close button
                             setShowModal(false);
                             props.onClose();
                         }}>Close
                     </Button>
 
-                    <Button variant="primary" /* onClick={closeModal} */>Save changes</Button>
+                    <Button variant="primary" onClick={updateHandler}>Save changes</Button>
                 </Modal.Footer>
             </Modal>
         </div>
